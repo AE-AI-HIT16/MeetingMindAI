6408312a2f73dac8368a38fa57ae4c6255fd3093
Lanlanchuche <lanlannguyen3306@gmail.com>
2026-07-02 21:24:05 +0700

    fix: modify init file of dataset loader

---
+++ meetasr/datasets/__init__.py
@@ -0,0 +1,6 @@
+"""Dataset loaders for MeetASR."""
+
+from meetasr.datasets.scp_dataset import SCPDataset
+from meetasr.datasets.jsonl_dataset import JSONLDataset
+
+__all__ = ["SCPDataset", "JSONLDataset"]
